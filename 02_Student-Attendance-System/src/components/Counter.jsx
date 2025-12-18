44347b448ddd2c47866e1d836ac272a9c606a5e6
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2025-12-18 18:55:05 +0600

    feat(counter): add variable step functionality
    - Refactor reducer for action objects
    - Enable variable increment/decrement
    - Add buttons for step of 1 and 5
    - Adjust buttons

--- 02_Student-Attendance-System/src/components/Counter.jsx
+++ 02_Student-Attendance-System/src/components/Counter.jsx
@@ -1,11 +1,13 @@
 import { useReducer } from "react";
 
 const counterReducer = (state, action) => {
-  if (action === "Increase") {
-    return state + 1;
-  }
-  if (action === "Decrease") {
-    return state - 1;
+  switch (action.type) {
+    case "increase":
+      return state + action.payload;
+    case "decrease":
+      return state - action.payload;
+    default:
+      return state;
   }
 };
 
@@ -16,8 +18,46 @@ const Counter = () => {
     <div className="counter-container">
       <h2>Counter App</h2>
       <p>The counter is {counter}</p>
-      <button onClick={() => dispatch("Increase")}>Increase by 1</button>
-      <button onClick={() => dispatch("Decrease")}>Decrease by 1</button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "increase",
+            payload: 1,
+          })
+        }
+      >
+        Increase by 1
+      </button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "decrease",
+            payload: 1,
+          })
+        }
+      >
+        Decrease by 1
+      </button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "increase",
+            payload: 5,
+          })
+        }
+      >
+        Increase by 5
+      </button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "decrease",
+            payload: 5,
+          })
+        }
+      >
+        Decrease by 5
+      </button>
     </div>
   );
 };
