8572d87a1187fd9fd52457013e43f74684026f19
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2025-12-21 13:04:53 +0600

    feat(board): add board reducer with core logic
    - Implement board creation and management
    - Add logic for renaming and removing boards
    - Manage list IDs within board state
    - Manage task IDs within task state

---
+++ 04_Project-Management-App/src/reducers/board.js
@@ -0,0 +1,81 @@
+export const boardReducer = (boards = [], action) => {
+  switch (action.type) {
+    case "CREATE_BOARD": {
+      const newBoard = {
+        id: Date.now() + "",
+        title: action.payload,
+        lists: [],
+        tasks: [],
+      };
+      return [...boards, newBoard];
+    }
+    case "CHANGE_BOARD_NAME": {
+      return boardReducer.map((item) => {
+        if (item.id === action.payload.id) {
+          return {
+            ...item,
+            title: action.payload.title,
+          };
+        }
+        return item;
+      });
+    }
+    case "REMOVE_BOARD": {
+      return boards.filter((item) => item.id !== action.payload.id);
+    }
+    case "ADD_LIST_ID": {
+      const updatedBoards = boards.map((item) => {
+        if (item.id === action.payload.id) {
+          return {
+            ...item,
+            lists: [...item.lists, action.payload.listId],
+          };
+        }
+        return item;
+      });
+      return updatedBoards;
+    }
+    case "REMOVE_LIST_ID": {
+      const updatedBoards = boards.map((item) => {
+        if (item.id === action.payload.id) {
+          return {
+            ...item,
+            lists: item.lists.filter(
+              (listId) => listId !== action.payload.listId
+            ),
+          };
+        }
+        return item;
+      });
+      return updatedBoards;
+    }
+    case "ADD_TASK_ID": {
+      const updatedBoards = boards.map((item) => {
+        if (item.id === action.payload.id) {
+          return {
+            ...item,
+            tasks: [...item.tasks, action.payload.taskId],
+          };
+        }
+        return item;
+      });
+      return updatedBoards;
+    }
+    case "REMOVE_TASK_ID": {
+      const updatedBoards = boards.map((item) => {
+        if (item.id === action.payload.id) {
+          return {
+            ...item,
+            tasks: item.tasks.filter(
+              (taskId) => taskId !== action.payload.taskId
+            ),
+          };
+        }
+        return item;
+      });
+      return updatedBoards;
+    }
+    default:
+      return boards;
+  }
+};
