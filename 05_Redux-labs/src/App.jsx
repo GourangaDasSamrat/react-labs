4437715cce28b67d3568d02523f031f9f7fbd642
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2025-12-24 17:32:13 +0600

    feat(theme): add theme management and update counter logic
    - Implement theme reducer for background and text colors
    - Add UI controls to change and reset theme in App
    - Apply theme styles to

--- 05_Redux-labs/src/App.jsx
+++ 05_Redux-labs/src/App.jsx
@@ -2,7 +2,8 @@ import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 
 const App = () => {
-  const counter = useSelector((storeState) => storeState);
+  const counter = useSelector((storeState) => storeState.counter);
+  const theme = useSelector((storeState) => storeState.theme);
   const dispatch = useDispatch();
 
   const handleIncrease = (payload) => {
@@ -11,21 +12,56 @@ const App = () => {
   const handleDecrease = (payload) => {
     dispatch({ type: "counter/decrement", payload: payload });
   };
+  const handleChangeBG = (payload) => {
+    dispatch({ type: "theme/CHANGE_BG_COLOR", payload: payload });
+  };
+  const handleChangeText = (payload) => {
+    dispatch({ type: "theme/CHANGE_TEXT_COLOR", payload: payload });
+  };
   return (
-    <div>
-      <p>The value of counter is {counter}</p>
-      <button type="button" onClick={() => handleIncrease(1)}>
-        Increment by 1
-      </button>
-      <button type="button" onClick={() => handleDecrease(1)}>
-        Decrement by 1
-      </button>
-      <button type="button" onClick={() => handleIncrease(5)}>
-        Increment by 5
-      </button>
-      <button type="button" onClick={() => handleDecrease(5)}>
-        Decrement by 5
-      </button>
+    <div
+      style={{
+        backgroundColor: theme.bgColor,
+        color: theme.textColor,
+      }}
+    >
+      <div>
+        <p>The value of counter is {counter}</p>
+        <button type="button" onClick={() => handleIncrease(1)}>
+          Increment by 1
+        </button>
+        <button type="button" onClick={() => handleDecrease(1)}>
+          Decrement by 1
+        </button>
+        <button type="button" onClick={() => handleIncrease(5)}>
+          Increment by 5
+        </button>
+        <button type="button" onClick={() => handleDecrease(5)}>
+          Decrement by 5
+        </button>
+      </div>
+      <br />
+      <div>
+        <div>
+          <button type="button" onClick={() => handleChangeBG("tomato")}>
+            Change bg color to tomato
+          </button>
+          <button type="button" onClick={() => handleChangeBG("purple")}>
+            Change bg color to purple
+          </button>
+        </div>
+        <div>
+          <button type="button" onClick={() => handleChangeText("tomato")}>
+            Change text color to tomato
+          </button>
+          <button type="button" onClick={() => handleChangeText("purple")}>
+            Change text color to purple
+          </button>
+          <button type="button" onClick={dispatch({ type: "theme/RESET" })}>
+            Reset theme
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- 05_Redux-labs/src/store/index.js
+++ 05_Redux-labs/src/store/index.js
@@ -1,14 +1,43 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
 const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case "counter/increment":
       return state + action.payload;
     case "counter/decrement":
-      return state + action.payload;
+      return state - action.payload;
     default:
       return state;
   }
 };
 
-export const store = createStore(counterReducer);
+const initState = {
+  bgColor: "#fff",
+  textColor: "#000",
+};
+
+const themeReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "theme/CHANGE_BG_COLOR":
+      return {
+        ...state,
+        bgColor: action.payload,
+      };
+    case "theme/CHANGE_TEXT_COLOR":
+      return {
+        ...state,
+        textColor: action.payload,
+      };
+    case "theme/RESET":
+      return initState;
+    default:
+      return state;
+  }
+};
+
+const root = combineReducers({
+  counter: counterReducer,
+  theme: themeReducer,
+});
+
+export const store = createStore(root);
