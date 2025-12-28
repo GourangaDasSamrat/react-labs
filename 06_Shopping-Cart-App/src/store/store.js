d429b55261372689f19130291fad73402e1d8bf9
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2025-12-28 13:50:24 +0600

    feat(cart): implement cart management reducer
    - Create cart reducer with core logic
    - Integrate cart reducer into main store
    - Handle add, remove, and modify quantity
    - Implement clear cart functionality

--- 06_Shopping-Cart-App/src/store/store.js
+++ 06_Shopping-Cart-App/src/store/store.js
@@ -1,7 +1,7 @@
 import { combineReducers, createStore } from "redux";
-
+import cartReducer from "./reducers/cart";
 const rootReducer = combineReducers({
-
+  cart: cartReducer,
 });
 
 export const store = createStore(
