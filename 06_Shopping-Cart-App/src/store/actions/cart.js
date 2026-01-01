09aed5277e0249833b8db0b4bafdfce46dea33c3
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-01-01 12:35:11 +0600

    refactor(cart): migrate actions to Redux Toolkit
    - Convert action creators to use createAction
    - Remove old cart reducer file

--- 06_Shopping-Cart-App/src/store/actions/cart.js
+++ 06_Shopping-Cart-App/src/store/actions/cart.js
@@ -1,26 +1,6 @@
-export const addToCart = (product) => {
-  return {
-    type: "cart/addToCart",
-    payload: product,
-  };
-};
+import { createAction } from "@reduxjs/toolkit";
 
-export const modifyQuantity = (obj) => {
-  return {
-    type: "cart/modifyQuantity",
-    payload: obj,
-  };
-};
-
-export const removeProduct = (prodId) => {
-  return {
-    type: "cart/removeProduct",
-    payload: prodId,
-  };
-};
-
-export const clearCart = () => {
-  return {
-    type: "cart/clearCart",
-  };
-};
+export const addToCart = createAction("cart/addToCart");
+export const modifyQuantity = createAction("cart/modifyQuantity");
+export const removeProduct = createAction("cart/removeProduct");
+export const clearCart = createAction("cart/clearCart");
