1e8181e6b145888711781a342acf7555a311737c
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-01-29 20:25:41 +0600

    feat(cart): add shopping cart functionality
    - Implement Redux slice for cart management
    - Add actions for item add, remove, and update
    - Include action to clear the entire cart
    - Consolidate cart slice export

---
+++ 09_Ecommerce-App/src/features/cart.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {
+    addTodoCart(state, action) {
+      const product = state.find((item) => item.id === action.payload.id);
+      product
+        ? product.quantity++
+        : state.push({ ...action.payload, quantity: 1 });
+    },
+
+    removeItemFromCart(state, action) {
+      return state.filter((item) => item.id === action.payload);
+    },
+
+    modifyQuantityOfAnItem(state, action) {
+      const product = state.find((item) => item.id === action.payload.id);
+      product.quantity = action.payload.quantity;
+    },
+
+    clearCart() {
+      return [];
+    },
+  },
+});
+
+export const {
+  addTodoCart,
+  removeItemFromCart,
+  modifyQuantityOfAnItem,
+  clearCart,
+} = cartSlice.actions;
