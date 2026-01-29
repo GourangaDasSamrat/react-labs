4d0b5c4a0d5b275d48d97140e4f848e5f19c64fd
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-01-29 18:51:03 +0600

    feat(store): initialize Redux store
    - Created store configuration file
    - Integrated Redux Provider in main.jsx
    - Added cartSlice reducer to store

--- 09_Ecommerce-App/src/main.jsx
+++ 09_Ecommerce-App/src/main.jsx
@@ -1,12 +1,16 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { store } from "./app/store.js";
 import "./index.css";
 import { rootRouter } from "./routers/router.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={rootRouter} />
+    <Provider store={store}>
+      <RouterProvider router={rootRouter} />
+    </Provider>
   </StrictMode>,
 );
