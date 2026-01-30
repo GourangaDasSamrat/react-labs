0ed736ea78f1ba4dbfb3fc4470475e5e2d2dad8a
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-01-30 12:46:35 +0600

    feat(pages): add sign-in and sign-up pages

--- 09_Ecommerce-App/src/pages/index.js
+++ 09_Ecommerce-App/src/pages/index.js
@@ -2,3 +2,5 @@ export { default as AddProduct } from "./AddProduct";
 export { default as Cart } from "./Cart";
 export { default as NotFound } from "./NotFound";
 export { default as Shop } from "./Shop";
+export { default as SignIn } from "./SignIn";
+export { default as SignUp } from "./SignUp";
