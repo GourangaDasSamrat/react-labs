b1ff95dcc1daa83220416ea42ada2745646c746c
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-02 18:24:36 +0600

    feat(store): configure Redux with API slice
    - Import `apiSlice` from features
    - Add `apiSlice` reducer to store
    - Integrate `apiSlice` middleware

--- 09_Ecommerce-App/src/app/store.js
+++ 09_Ecommerce-App/src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { cartSlice } from "../features";
+import { apiSlice, cartSlice } from "../features";
 
 export const store = configureStore({
   reducer: {
     cart: cartSlice.reducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
