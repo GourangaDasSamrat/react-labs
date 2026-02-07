6f7669c8ea00491c1520d0ca013c1d10c679fbd2
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-07 11:08:05 +0600

    refactor(components): move Navbar component
    - Relocate Navbar.jsx to app/components
    - Update import path in layout.js
    - Correct whitespace for Navbar usage

--- 10_basic-next-js/src/app/layout.js
+++ 10_basic-next-js/src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "./components/Navbar";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Navbar/>
+        <Navbar />
         {children}
       </body>
     </html>
